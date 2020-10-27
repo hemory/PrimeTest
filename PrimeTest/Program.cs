0c3dc1b9b4110040fcb1ef57aab65b4d951f83ae
Hemory Phifer <hemphife@gmail.com>
2020-10-27 08:45:41 -0400

    new commit

---
+++ PrimeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
